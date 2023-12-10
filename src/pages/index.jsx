3c677e18f6e389bffb20cd3f0d8676c870265c43
ANIS&EMIL <ANIS&EMIL@DESKTOP-OJT85QL>
2023-12-10 17:00:05 +0600

    added comment section and contuct us section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,31 @@
-import { Box, Typography, Grid, Card } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  Avatar,
+  Rating,
+  TextField,
+  Button,
+} from "@mui/material";
 import { motion, useInView } from "framer-motion";
-import { use, useRef } from "react";
+import { useRef } from "react";
 import Image from "next/image";
 
 export default function Home() {
   const ref = useRef(null);
   const ref1 = useRef(null);
   const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
 
   const isInView = useInView(ref, { once: true });
   const isInView1 = useInView(ref1, { once: true });
   const isInView2 = useInView(ref2, { once: true });
+  const isInView3 = useInView(ref3, { once: true });
+  const isInView4 = useInView(ref3, { once: true });
 
   const logoVariants = {
     hidden: { opacity: 0, x: 60 },
@@ -34,6 +49,63 @@ export default function Home() {
     {
       header: "Доставка по всей Астане",
       body: "Медленный транспорт не нужен когда время тикает",
+    },
+  ];
+
+  const comments = [
+    {
+      nickName: "​Ернар Ибраев",
+      comment:
+        "Беру на свою элантру запчасти только здесь! Ходовка, свечи и т.д., все качественное. Машина чувствует себя прекрасно, разве что не летает! Спасибо Артуру, за клиентоориентированность, всегда грамотно проконсультирует, подскажет что лучше. Зачем ехать на базар, когда рядом такой магазинчик есть) Цены низкие, сравниваю с другими в интернете. Что ещё надо для счастья!",
+      rating: 5,
+    },
+    {
+      nickName: "​Дархан Магауин",
+      comment:
+        "Спасибо за помощь. Проконсультировали меня с добротой и пониманием, хотя уже было время для закрытия. Подсказали какую запчасть лучше установить, хозяин знает в автомобилях буквально всё. Так еще и установку модно сделать прямо напротив магазина, что очень удобно.",
+      rating: 4.5,
+    },
+    {
+      nickName: "K KZ",
+      comment:
+        "Хозяин молодец. Обслуживание хорошее. Цены приземлённые, адекватные. Всем советую!",
+      rating: 4,
+    },
+    {
+      nickName: "David Avakyan",
+      comment:
+        "Колодки вместе со свечами купил недавно в этом магазине, все отлично, продавец толковый, свое дело знает",
+      rating: 5,
+    },
+    {
+      nickName: "Elizaveta Mil",
+      comment:
+        "Купила свечи 4 штуки, на свой Киа церато, все отлично работает, уже 2 недели прошло, двигатель как часы работает заводиться с пол оборота",
+      rating: 4.5,
+    },
+    {
+      nickName: "Olga Trofimova",
+      comment:
+        "Большой ассортимент, помогли мне, честно не знаю что покупала в сто дали список и пошла купила,цены приемлемые, продавец вежливый",
+      rating: 5,
+    },
+    {
+      nickName: "Яна Хан",
+      comment:
+        "Купила колпаки на свой акцент, все отлично, помог установить, огромное человеческое спасибо",
+      rating: 4.5,
+    },
+    {
+      nickName: "Марина Яковлева",
+      comment:
+        "Советую, магазин отличный, купила антифриз, Все отлично, вежливый продавец",
+      rating: 5,
+    },
+    {
+      nickName: "​Марина Морозова",
+      comment:
+        "Очень богатый выбор! Стану постоянным покупателем! Также очень приветливые и знающие свое дело продавцы! цены умеренные! вообщем меня все устроило! буду заходить почаще! всем удачи!",
+      rating: 5,
     },
   ];
 
@@ -379,6 +451,170 @@ export default function Home() {
         >
           <Image alt="engine" src="/gasket.png" width={500} height={480} />
         </Card>
+      </Box>
+      <Box
+        ref={ref3}
+        className="comments"
+        sx={{
+          width: "calc(100% - 300px)",
+          height: "auto",
+          padding: "100px 150px 100px 150px",
+          backgroundColor: "#EF233C",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+        component={motion.div}
+        initial="hidden"
+        animate={isInView3 ? "visible" : ""}
+        transition={{
+          duration: 2,
+          times: [0, 0.5, 1],
+          ease: "backInOut",
+          staggerChildren: 0.3,
+          childrenDelay: 0.4,
+        }}
+      >
+        {comments.map((element, index) => {
+          return (
+            <>
+              <Card
+                key={index}
+                sx={{
+                  backgroundColor: "white",
+                  opacity: 0.87,
+                  width: "320px",
+                  height: "auto",
+                  marginTop: "20px",
+                  marginLeft: "20px",
+                  borderRadius: "20px",
+                  padding: "30px",
+                }}
+                component={motion.div}
+                variants={{
+                  hidden: { opacity: 0, y: 60 },
+                  visible: { opacity: [0, 0.4, 0.87], y: [60, 0, 0] },
+                }}
+              >
+                <CardHeader
+                  sx={{ marginBottom: "4px" }}
+                  avatar={
+                    <Avatar>
+                      <Image
+                        src={`/user${index}.png`}
+                        alt={`user${index}`}
+                        fill
+                      />
+                    </Avatar>
+                  }
+                  title={
+                    <Typography varaint="WixExtraBold" fontSize="20px">
+                      {element.nickName}
+                    </Typography>
+                  }
+                  subheader={
+                    <Rating
+                      name="commentRating"
+                      precision={0.5}
+                      value={element.rating}
+                      size="small"
+                      readOnly
+                    />
+                  }
+                />
+                <CardContent>
+                  <Typography variant="InterMedium" fontSize="16px">
+                    {element.comment}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </>
+          );
+        })}
+      </Box>
+      <Box
+        className="contactUs"
+        sx={{
+          width: "calc(100% - 300px)",
+          height: "auto",
+          padding: "100px 150px 200px 150px",
+          backgroundColor: "#DADEE5",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          ref={ref4}
+          sx={{
+            alignSelf: "center",
+            marginBottom: "40px",
+            color: "#001122",
+            position: "relative",
+            left: "10px",
+          }}
+          component={motion.span}
+          {...commonTypography}
+          animate={isInView4 ? "visible" : ""}
+        >
+          {wordLeftAnimation("Свяжитесь с нами", "InterBold", "50px", {
+            marginRight: "15px",
+          })}
+        </Typography>
+        <Box
+          sx={{
+            height: "290px",
+            width: "auto",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignSelf: "center",
+            }}
+          >
+            {["Имя", "Email"].map((placeHolder, index) => {
+              return (
+                <>
+                  <TextField
+                    key={index}
+                    label={placeHolder}
+                    variant="outlined"
+                    required
+                    sx={{ width: "265px", marginLeft: "15px" }}
+                  />
+                </>
+              );
+            })}
+          </Box>
+          <TextField
+            label="Сообщение"
+            multiline
+            minRows={10}
+            sx={{
+              width: "44.5%",
+              borderRadius: "10px",
+              marginTop: "15px",
+              position: "relative",
+              left: "344px",
+              opacity: 0.5,
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              width: "540px",
+              alignSelf: "center",
+              position: "relative",
+              left: "6px",
+              marginTop: "10px",
+            }}
+          >
+            <Typography variant="SchibstedRegular">Отправить</Typography>
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
